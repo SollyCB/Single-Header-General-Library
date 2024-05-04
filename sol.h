857faa21d3b23eda5642c7990c6353457b8c61ad
SollyCB <solomoncardenbrown@gmail.com>
2024-05-04 19:32:09 +0100

    add mmap ringbuffer thingy

--- sol.h
+++ sol.h
@@ -33,8 +33,28 @@
 #include <pthread.h>
 #endif
 
-// @Todo
-// math.h <- this is currently under construction elsewhere
+/* @Todo
+    - math.h <- this is currently under construction elsewhere
+
+    - copy/set routines, e.g.
+
+inline static void copy_small(void *to, void *from, size_t size)
+{
+    uchar *t = to;
+    uchar *f = from;
+    for(size_t i=0; i < size; ++i)
+        t[i] = from[i];
+}
+
+inline static void copy_small_16x(void *to, void *from, size_t size)
+{
+    size >> 4;
+    for(size_t i=0; i < size; ++i) {
+        __m128i a = _mm_load_si128((__m128i*)((uchar*)from + (i<<4)));
+        _mm_store_si128((__m128i*)((uchar*)to + (i<<4)), a);
+    }
+}
+*/
 
 //                                  ## BEGIN FILE ##
 
@@ -171,6 +191,7 @@ static inline void memcpy_if(void *to, const void *from, size_t size, bool b) {
 #define smemcpy(to, from, type, count) memcpy(to, from, sizeof(type) * count)
 #define smemmove(to, from, type, count) memmove(to, from, sizeof(type) * count)
 #define carrlen(a) (sizeof(a)/sizeof(a[0]))
+#define cstrarrlen(a) ((sizeof(a)/sizeof(a[0])) - 1)
 #define cstr_as_array_len(a) ((sizeof(a)/sizeof(a[0])) - 1)
 
 #define smalloc(cnt, type) malloc(cnt * sizeof(type))
@@ -856,7 +877,7 @@ typedef enum {
 
 typedef struct allocator allocator;
 typedef void* (*allocator_allocate)  (allocator*, size_t);
-typedef void* (*allocator_reallocate)(allocator *, void *, size_t, size_t);
+typedef void* (*allocator_reallocate)(allocator*, void *, size_t, size_t);
 typedef void  (*allocator_deallocate)(allocator*, void*);
 
 struct allocator {
@@ -932,6 +953,25 @@ static inline void allocator_reset_linear_to(allocator *alloc, uint64 to) {
     if (alloc->flags & ALLOCATOR_LINEAR_BIT)
         alloc->linear.used = to;
 }
+
+////////////////////////////////////////////////////////////////////////////////
+// ringbuffer.h
+
+enum {
+    RB_NOTAIL = 0x01,
+};
+
+typedef struct ringbuffer {
+    uchar *data;
+    uint flags;
+    int size;
+    int head;
+    int tail;
+} ringbuffer;
+
+bool init_ringbuffer(ringbuffer *buf, uint size, uint flags);
+void* rballoc(ringbuffer *rb, uint size);
+bool rbfree(ringbuffer *rb, uint size);
 
 ////////////////////////////////////////////////////////////////////////////////
 // array.h
@@ -1965,6 +2005,19 @@ static inline void prog_timer_add(program_timer *pt, float t, string *fn)
 
 void dump_prog_timer(program_timer *pt, const char *file, allocator *alloc);
 
+// @NOTE: I have a feeling that these timers are rubbish but not sure...
+#define NS_TIMER_START(timer) struct timespec timer = get_time_cpu_proc();
+#define NS_TIMER_STOP(timer) size_t ns = get_time_cpu_proc().tv_nsec - timer.tv_nsec;
+
+#define SIMPLE_TIMER_START(timer) \
+    struct timespec timer = get_time_cpu_proc()
+#define SIMPLE_TIMER_STOP(timer) \
+    do { \
+        struct timespec m__t_stop = get_time_cpu_proc(); \
+        m__t_stop = diffts(&m__t_stop, &timer);          \
+        print_ts_float(&m__t_stop);                      \
+    } while(0)
+
 #define BT_START() \
     struct timespec m__t_##__COUNTER__ = get_time_cpu_thread()
 #define BT_STOP(bt)                                                    \
@@ -2842,6 +2895,112 @@ void store_array(const char *fname, void *a)
     struct allocation alloc = array_allocation(a);
     fwrite(alloc.data,1,alloc.size,f);
     fclose(f);
+}
+
+//////////////////////////////////////////////////////////////
+// ringbuffer.c
+
+bool init_ringbuffer(ringbuffer *buf, uint size, uint flags)
+{
+    size = align(size, getpagesize());
+    assert(size < Max_s32);
+
+    void *m = mmap(NULL, size * 2, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANON, -1, 0);
+    if (m == MAP_FAILED) {
+        log_print_error("failed to map data, size %u", size * 2);
+        return false;
+    }
+
+    int fd = memfd_create("ringbuffer", 0x0);
+    if(fd == -1) {
+        log_print_error("failed to memfd_create");
+        goto fail_fd_create;
+    }
+
+    if (ftruncate(fd, size) == -1) {
+        log_print_error("failed to truncate file - size %u", size);
+        goto fail_truncate;
+    }
+
+    void *p1 = mmap((uchar*)m + 0,    size, PROT_READ|PROT_WRITE, MAP_SHARED|MAP_FIXED, fd, 0);
+    void *p2 = mmap((uchar*)m + size, size, PROT_READ|PROT_WRITE, MAP_SHARED|MAP_FIXED, fd, 0);
+
+    if (p1 == MAP_FAILED || p2 == MAP_FAILED) {
+        log_print_error("failed to map pages to file");
+        goto fail_truncate;
+    }
+
+    buf->size = size;
+    buf->data = m;
+    buf->flags = flags;
+    buf->head = 0;
+    buf->tail = 0;
+
+    return true;
+
+fail_truncate:
+    close(fd);
+fail_fd_create:
+    munmap(m, size*2);
+    return false;
+}
+
+static inline bool rb_head_crosses_tail(ringbuffer *rb, uint size)
+{
+    assert(rb->head < Max_s32 - size);
+    // @Todo There *must* be a nice algorithm to find this without branches.
+    int h = rb->head;
+    int t = rb->tail;
+    if (h > t && h + size - rb->size > t)
+        return true;
+    if (h < t && h + size > t)
+        return true;
+    return false;
+}
+
+static inline bool rb_tail_crosses_head(ringbuffer *rb, uint size)
+{
+    assert(rb->tail < Max_s32 - size);
+    // @Todo There *must* be a nice algorithm to find this without branches.
+    int t = rb->tail;
+    int h = rb->head;
+    if (t > h && t + size - rb->size > h)
+        return true;
+    if (t < h && t + size > h)
+        return true;
+    return false;
+}
+
+void* rballoc(ringbuffer *rb, uint size)
+{
+    size = allocalign(size);
+    assert(size <= rb->size);
+
+    if (!(rb->flags & RB_NOTAIL) && rb_head_crosses_tail(rb, size)) {
+        log_print_error("ringbuffer overflow, head would cross tail: allocation size = %u, rb = {.size = %u, .head = %u, .tail = %u}",
+                        size, rb->size, rb->head, rb->tail);
+        return NULL;
+    }
+
+    void *ret = rb->data + rb->head;
+    rb->head += size;
+    rb->head -= rb->size & maxif(rb->head >= rb->size);
+
+    return ret;
+}
+
+bool rbfree(ringbuffer *rb, uint size)
+{
+    if (!(rb->flags & RB_NOTAIL) && rb_tail_crosses_head(rb, size)) {
+        log_print_error("ringbuffer underflow, tail would cross head: allocation size = %u, rb = {.size = %u, .head = %u, .tail = %u}",
+                        size, rb->size, rb->head, rb->tail);
+        return 0;
+    }
+
+    rb->tail += size;
+    rb->tail -= rb->size & maxif(rb->tail >= rb->size);
+
+    return true;
 }
 
 //////////////////////////////////////////////////////////////
